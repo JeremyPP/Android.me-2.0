9bbc2220be9f1ccded0e686139f6734818cc95ba
JeremyPP <jeremy.petit@ccc.ufcg.edu.br>
2015-07-27 19:34:28 +0100

    add mising js file

---
+++ main.js
@@ -0,0 +1,22 @@
+﻿
+/* Header bar shadow on scroll */
+
+$(window).scroll(function() {
+    if($(window).scrollTop() > 125) {
+        $(".page-header").css('box-shadow', '0 2px 5px rgba(0,0,0,.26)');
+    } else{
+        $(".page-header").css('box-shadow', 'none');
+    }
+});
+
+
+/* Open/close responsive menu */
+
+$(".resp-name-logo").click(function(){
+    $(".header-menu").css("marginLeft", "0");
+    $(".responsive-menu-overlay").css("display", "block");
+});
+$(".responsive-menu-overlay").click(function(){
+    $(".header-menu").css("marginLeft", "-280px");
+    $(".responsive-menu-overlay").css("display", "none");
+});
